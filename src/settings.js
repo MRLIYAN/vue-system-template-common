723eca123797e424cefa6db536dfcfe778d861fe
liyan <li-yan@bmsoft.com.cn>
2020-12-26 17:34:33 +0800

    上传settings.js

---
+++ src/settings.js
@@ -0,0 +1,14 @@
+export default {
+    /**
+     *@type:String 
+     *@description 系统默认名字，浏览器标题
+    */
+    title:"vue-system-template",
+
+    /**
+     *@type:Boolean 
+     *@description 控制面包屑是否显示
+    */
+    breadCrumb:true,
+    
+}
